2a195866a20b8c429a8a45545be34fdca39d6f0a
BolzanoLeBo <loic.thomas1509@gmail.com>
2024-01-15 15:29:05 +0100

    plot better

--- test_dataset.py
+++ test_dataset.py
@@ -1,32 +1,33 @@
 import numpy as np
-import matplotlib . pyplot as plt
-from scipy . io import arff
+import matplotlib.pyplot as plt
+from scipy.io import arff
 import time
 from sklearn import cluster
+import scipy.cluster.hierarchy as shc
 # Parser un fichier de donnees au format arff
 # data est un tableau d ’ exemples avec pour chacun
 # la liste des valeurs des features
 #
 # Dans les jeux de donnees consideres :
-# il y a 2 features ( dimension 2 )
-# Ex : [[ - 0 . 499261 , -0 . 0612356 ] ,
-# [ - 1 . 51369 , 0 . 265446 ] ,
-# [ - 1 . 60321 , 0 . 362039 ] , .....
+# il y a 2 features(dimension 2 )
+# Ex : [[ - 0.499261 , -0.0612356 ] ,
+# [ - 1.51369 , 0.265446 ] ,
+# [ - 1.60321 , 0.362039 ] , .....
 # ]
 #
 # Note : chaque exemple du jeu de donnees contient aussi un
-# numero de cluster . On retire cette information
+# numero de cluster.On retire cette information
 path = '../clustering-benchmark/src/main/resources/datasets/artificial/'
-databrut = arff.loadarff(open( path + "xclara.arff" , 'r') )
+databrut = arff.loadarff(open( path +"xclara.arff", 'r') )
 datanp = [ [x[0] ,x[1]] for x in databrut [ 0 ] ]
 # Affichage en 2D
 # Extraire chaque valeur de features pour en faire une liste
-# Ex pour f0 = [ - 0 . 499261 , -1 . 51369 , -1 . 60321 , ...]
-# Ex pour f1 = [ - 0 . 0612356 , 0 . 265446 , 0 . 362039 , ...]
+# Ex pour f0 = [ - 0.499261 , -1.51369 , -1.60321 , ...]
+# Ex pour f1 = [ - 0.0612356 , 0.265446 , 0.362039 , ...]
 f0 = [x[0] for x in datanp]  # tous les elements de la premiere colonne
 f1 = [x[1] for x in datanp] # tous les elements de la deuxieme colonne
 
-print ( " Appel KMeans pour une valeur fixee de k " )
+print("Appel KMeans pour une valeur fixee de k")
 tps1 = time.time ()
 k = 3
 model = cluster.KMeans(n_clusters =k , init ='k-means++')
@@ -48,4 +49,8 @@ plt.title("Données initiales")
 plt.subplot(1, 2, 2)  # 1 row, 2 columns, second subplot
 plt.scatter(f0, f1, c=labels, s=8)
 plt.title("Données après clustering KMeans")
+
 plt.show()
+
+
+
