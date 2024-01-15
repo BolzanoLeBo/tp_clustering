f209c7908cbdfb8928ab0b8d1efa647304ef9268
BolzanoLeBo <loic.thomas1509@gmail.com>
2024-01-15 15:28:07 +0100

    plot better

--- test_dataset_agglo.py
+++ test_dataset_agglo.py
@@ -43,84 +43,79 @@ plt.plot()
 '''
 # set distance_threshold(0 ensures we compute the full tree )
 #silhouette calvinski davies 
-scores_name= ["silhouette", "calvinski", "davies"]
-best_results = [[0],[0],[0]]
-scores = [[], [], []]
-dist_max = 100
-step = 10
-distances = np.arange(1, dist_max + 1, step)
-for d in distances: 
-    print(d/dist_max)
-    tps1 = time.time ()
-    model = cluster.AgglomerativeClustering(distance_threshold = d ,linkage = 'complete' , n_clusters = None )
-    model = model.fit(datanp )
-    tps2 = time.time ()
-    labels = model.labels_
-    k = model.n_clusters_
-    leaves = model.n_leaves_
-    #check score
+linkage_l = ['single', 'average', 'complete', 'ward']
+for l in linkage_l : 
+    scores_name= ["silhouette", "calvinski", "davies"]
+    best_results = [[0],[0],[0]]
+    scores = [[], [], []]
+    dist_max = 1000
+    step = 10
+    distances = np.arange(1, dist_max + 1, step)
+    for d in distances: 
+        print(d/dist_max)
+        tps1 = time.time ()
+        model = cluster.AgglomerativeClustering(distance_threshold = d ,linkage = l , n_clusters = None )
+        model = model.fit(datanp )
+        tps2 = time.time ()
+        labels = model.labels_
+        k = model.n_clusters_
+        leaves = model.n_leaves_
+        #check score
+        if k > 1 : 
+            silhouette = silhouette_score(datanp, labels)
+            calinski = calinski_harabasz_score(datanp, labels)
+            davies = davies_bouldin_score(datanp, labels)
 
-    silhouette = silhouette_score(datanp, labels)
-    calinski = calinski_harabasz_score(datanp, labels)
-    davies = davies_bouldin_score(datanp, labels)
+            scores[0].append(silhouette)
+            scores[1].append(calinski)
+            scores[2].append(davies)
+        else : 
 
-    scores[0].append(silhouette)
-    scores[1].append(calinski)
-    scores[2].append(davies)
+            silhouette = 0
+            calinski = 0
+            davies = 0
 
-
-    if silhouette >= best_results[0][0] :
-        best_results[0] = [silhouette, k, d, leaves, labels]
-    if calinski >= best_results[1][0] :
-        best_results[1] = [calinski, k, d, leaves, labels]
-    if davies >= best_results[2][0] :
-        best_results[2] = [davies, k, d, leaves, labels]
-
-# Affichage clustering
-plt.figure(figsize=(10,15))
-
-for i in range(len(best_results)) : 
-    [score, k, d, leaves, labels] = best_results[i]
-    plt.subplot(3,1,1+i)
-    plt.scatter(f0 , f1 , c = labels, s = 8 )
-    plt.title(f"distance = {d} / nb clusters ={k} / nb feuilles = {leaves} score {scores_name[i]}")
-    print("nb clusters =",k ,", nb feuilles =", leaves ,"runtime =", round (( tps2 - tps1 ) * 1000 , 2 ) ,"ms")
+            scores[0].append(0)
+            scores[1].append(0)
+            scores[2].append(0)
 
 
-# Plotting each set of scores
-plt.figure(figsize=(10, 6))
-plt.subplot(1,2,1)
-plt.plot(distances, scores[0], marker='o', label='Silhouette')
-plt.plot(distances, scores[2], marker='^', label='Davies-Bouldin')
+        if silhouette >= best_results[0][0] :
+            best_results[0] = [silhouette, k, d, leaves, labels]
+        if calinski >= best_results[1][0] :
+            best_results[1] = [calinski, k, d, leaves, labels]
+        if davies >= best_results[2][0] :
+            best_results[2] = [davies, k, d, leaves, labels]
 
-plt.xlabel('Distance Threshold')
-plt.ylabel('Score')
-plt.title('Clustering Scores by Distance Threshold')
-# Adding a legend
-plt.legend()
+    # Affichage clustering
+            
 
-plt.subplot(1,2,2)
-plt.plot(distances, scores[1], marker='s', label='Calinski-Harabasz')
-# Adding labels and title
-plt.xlabel('Distance Threshold')
-plt.ylabel('Score')
-plt.title('Clustering Scores by Distance Threshold')
-# Adding a legend
-plt.legend()
-print(scores[0][3])
-plt.show()
-'''
-# set the number of clusters
-k = 4
-tps1 = time.time ()
-model = cluster.AgglomerativeClustering(linkage = 'single' , n_clusters = 3)
-model = model.fit(datanp )
-tps2 = time.time ()
-labels = model.labels_
-kres = model.n_clusters_
-leaves = model.n_leaves_
+     # Plotting clustering results
+    plt.figure(figsize=(10, 15))
+    plt.suptitle(f"Clustering Results for Linkage: {l}")
+    for i in range(len(best_results)):
+        [score, k, d, leaves, labels] = best_results[i]
+        plt.subplot(3, 1, i+1)
+        plt.scatter(f0, f1, c=labels, s=8)
+        plt.title(f"distance = {d} / nb clusters = {k} / nb feuilles = {leaves} / score {scores_name[i]}")
+        print("nb clusters =", k, ", nb feuilles =", leaves, "runtime =", round((tps2 - tps1) * 1000, 2), "ms")
 
-plt.subplot(1,2,2)
-plt.scatter(f0 , f1 , c = labels , s = 8 )
-plt.title(f"Resultat du clustering avec {k} clusters ")
-print("nb clusters =",k ,", nb feuilles =", leaves ,"runtime =", round (( tps2 - tps1 ) * 1000 , 2 ) ,"ms")'''
+    # Plotting scores
+    plt.figure(figsize=(10, 6))
+    plt.suptitle(f"Clustering Scores for Linkage: {l}")
+    plt.subplot(1, 2, 1)
+    plt.plot(distances, scores[0], marker='o', label='Silhouette')
+    plt.plot(distances, scores[2], marker='^', label='Davies-Bouldin')
+    plt.xlabel('Distance Threshold')
+    plt.ylabel('Score')
+    plt.title('Silhouette & Davies-Bouldin Scores')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(distances, scores[1], marker='s', label='Calinski-Harabasz')
+    plt.xlabel('Distance Threshold')
+    plt.ylabel('Score')
+    plt.title('Calinski-Harabasz Scores')
+    plt.legend()
+
+    plt.show()
